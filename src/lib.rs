0b588048e5268a9308577ba759bda7a1f16d71af
shri-acha <happy.irhs@gmail.com>
2025-05-05 00:52:42 +0545

    Trying to send feature report for different interfaces

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,47 @@
-use std::str::Bytes;
-
 use hidapi;
+use rusb::Device;
 
-fn connect_to_mouse(){
+
+fn connect_to_mouse() -> hidapi::HidResult<()>{
 
     let mut data: [u8; 155] = [ 0 ; 155];
-    data[0x00] = 0x1c; // Report ID
-    data[0x88] = 0xFD; // RGB values
-    data[0x89] = 0xFE;
-    data[0x8A] = 0xFF;
+    data[0x00] = 0x4;
+    // bRequest = 9, no clue what this is 
+    // data[0x01] = 0x4;// ReportID = 04, this is a required field for two direction packet transfet
+    // data[0x02] = 0x3;// ReportType = 03, unsure
+    // data[0x03] = 0x1;//  wIndex = 1, data index
+    // data[0x06] = 0x9a;// wLength = 154 , length of data segment
 
+    data[0x65] = 0x22; // Something i believe
+    data[0x68] = 0x12; // Brightness level
+    data[0x6C] = 0xFF;// RGB values , RGB values +1 on the values because 0th value is not held by
+                      // requestID
+    data[0x6D] = 0xFF;
+    data[0x6E] = 0xFF;
+
+    let mut device_list = vec![];
     if let Ok(hid_ctx) = hidapi::HidApi::new(){
-        let m_handl = hid_ctx.open(0x258a,0x17).unwrap();
-        let _ = m_handl.read(&mut data); 
-        // let result = m_handl.send_feature_report(&data).unwrap();
+        for device in hid_ctx.device_list() {
+            device_list.push(device); 
+            println!("{:?} {:?}",device.interface_number(),device);
+            if device.vendor_id() == 9610 && device.product_id() == 23 && device.interface_number() == 1 {
+                if let Ok(m_handl) = hid_ctx.open(0x258a,0x17){
+                    if let Err(e) = m_handl.send_feature_report(&data){
+                        panic!("{}",e);
+                    }
+                    else{
+                        println!("Worked!");
+                        break;
+                    }
+                }else {
+                    println!("Didn't Work!");
+                }
+                break;
+            }
+        }
+        
     }
-
+    Ok(())
 }
 
 
@@ -25,6 +51,6 @@ mod tests {
 
     #[test]
     fn mouse_connection() {
-        let result = connect_to_mouse();
+        let _ = connect_to_mouse();
     }
 }

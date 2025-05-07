f51a04f6b3d5eed00b4054133b025d70ae78ae9c
shri-acha <happy.irhs@gmail.com>
2025-05-07 07:24:07 +0545

    added dpi bytes

--- src/lib.rs
+++ src/lib.rs
@@ -13,18 +13,27 @@ fn connect_to_mouse() -> hidapi::HidResult<()>{
     // data[0x03] = 0x1;//  wIndex = 1, data index
     // data[0x06] = 0x9a;// wLength = 154 , length of data segment
 
-    data[0x64] = 0x28; // 0x28 for steady mode. 
-    data[0x67] = 0x12; // Brightness level at Stale mode.
-    data[0x6B] = 0xFF;// RGB values , RGB values +1 on the values because -1th value is not held by requestID
-    data[0x6C] = 0xFF;
-    data[0x6D] = 0xFF;
+    data[0x08] = 0x30; // Constant data
 
-    data[0x94] = 0x58; // XY - 801 Footers
+    data[0x64] = 0x22; // 0x28 for steady mode. 
+    data[0x67] = 0x12; // Brightness level at Stale mode.
+
+
+    data[0x4A] = 01; // DPI (00 - 0a)
+    data[0x4B] = 02;
+    data[0x4C] = 04;
+
+    data[0x6A] = 0xFF;// RGB values , RGB values +1 on the values because -1th value is not held by requestID
+    data[0x6B] = 0xFF;
+    data[0x6C] = 0xFF;
+
+    data[0x94] = 0x58; // XY - 801 Signature
     data[0x95] = 0x59;
     data[0x96] = 0x2D;
     data[0x97] = 0x38;
     data[0x98] = 0x30;
     data[0x99] = 0x01;
+
 
     let mut device_list = vec![];
     if let Ok(hid_ctx) = hidapi::HidApi::new(){

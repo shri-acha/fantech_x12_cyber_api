64405da8dea764d84e9325a0ac4a8a05afadd290
shri-acha <happy.irhs@gmail.com>
2025-05-09 21:13:04 +0545

    Steady Mode, Blink Mode work, although color config doesn't

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,67 @@
 use hidapi;
-use rusb::Device;
 
 
 fn connect_to_mouse() -> hidapi::HidResult<()>{
 
     let mut data: [u8; 154] = [ 0 ; 154];
-    data[0x00] = 0x4;
 
-    // bRequest = 9, no clue what this is 
-    // data[0x01] = 0x4;// ReportID = 04, this is a required field for two direction packet transfet
-    // data[0x02] = 0x3;// ReportType = 03, unsure
-    // data[0x03] = 0x1;//  wIndex = 1, data index
-    // data[0x06] = 0x9a;// wLength = 154 , length of data segment
+    data[0x00] = 0x4; // Report ID
 
-    data[0x08] = 0x30; // Constant data
+    data[0x08] = 0x3c; // Constant data
 
-    data[0x64] = 0x22; // 0x28 for steady mode. 
-    data[0x67] = 0x12; // Brightness level at Stale mode.
+    data[0x4A] = 01; // DPI - 01 - (00 - 0a)
+    data[0x4B] = 02; // DPI - 02 - (00 - 0a)
+    data[0x4C] = 04; // DPI - 03 - (00 - 0a)
+    data[0x4D] = 08; // DPI - 04 - (00 - 0a) 
+
+    data[0x04E..0x05a].copy_from_slice(&[0x80;12]);  // Had a bunch of 0x80
 
 
-    data[0x4A] = 01; // DPI (00 - 0a)
-    data[0x4B] = 02;
-    data[0x4C] = 04;
+    data[0x5d] = 0x44; // 0x28 for steady mode or 0x22 for breathe mode
+    data[0x60] = 0x30; // Brightness level at steady mode or speed at breathe mode.
+    data[0x5f] = 0x01;
+    data[0x5e] = 0x0a;
 
-    data[0x6A] = 0xFF;// RGB values , RGB values +1 on the values because -1th value is not held by requestID
-    data[0x6B] = 0xFF;
-    data[0x6C] = 0xFF;
+
+    data[0x64] = 0xFF;// RGB values , 1st profile
+    data[0x65] = 0x00;
+    data[0x66] = 0x00;
+
+    data[0x67] = 0x00;// RGB values , 2nd profile 
+    data[0x68] = 0x00;
+    data[0x69] = 0xFF;
+
+    data[0x6a] = 0xFF;// RGB values , 3rd profile
+    data[0x6b] = 0x00;
+    data[0x6c] = 0xFF;
+
+    data[0x6d] = 0x00;// RGB values , 4th profile
+    data[0x6e] = 0x00;
+    data[0x6f] = 0xFF;
+   
+    //Constant Data
+    //
+    data[0x70] = 0xFF; 
+    data[0x71] = 0xFF;
+    data[0x74] = 0xFF;
+    data[0x75] = 0xFF;
+    data[0x76] = 0xFF;
+    data[0x77] = 0xFF;
+    data[0x78] = 0xFF;
+    data[0x7c] = 0xFF;
+    data[0x80] = 0xFF;
+    data[0x84] = 0xFF;
+    data[0x85] = 0xFF;
+    data[0x86] = 0xFF;
+    data[0x87] = 0xFF;
+    data[0x88] = 0xFF;
+    data[0x8a] = 0xFF;
+    data[0x8c] = 0xFF;
+    data[0x8d] = 0xFF;
+    data[0x8e] = 0xFF;
+    data[0x8f] = 0xFF;
+    data[0x90] = 0xFF;
+    // Not sure of the significance
 
     data[0x94] = 0x58; // XY - 801 Signature
     data[0x95] = 0x59;
@@ -34,6 +70,19 @@ fn connect_to_mouse() -> hidapi::HidResult<()>{
     data[0x98] = 0x30;
     data[0x99] = 0x01;
 
+    // Data being sent:
+    println!("Data being sent!");
+    let mut i:u8 = 0;
+    data.iter().for_each(|e|{
+        if i < 15 {
+            print!("{:?}  ",e);
+            i+=1;
+        }else {
+            print!("{:?}  ",e);
+            print!("\r\n");
+            i=0;
+        }
+    });
 
     let mut device_list = vec![];
     if let Ok(hid_ctx) = hidapi::HidApi::new(){
